b1d74a433418280855cf2a4a3b2d2aa01adb1e93
rafig <rjavadov7852@ada.edu.az>
2023-02-08 15:08:54 +0400

    v1.0.1

--- Backend/sdpprojectay2/src/main/java/com/java/sdpprojectay2/model/entity/Report.java
+++ Backend/sdpprojectay2/src/main/java/com/java/sdpprojectay2/model/entity/Report.java
@@ -1,5 +1,14 @@
 package com.java.sdpprojectay2.model.entity;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.stereotype.Indexed;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 public class Report {
-    
+    private Integer id;
+    private String name;
 }
