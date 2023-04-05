f4f3333e99d566d4405f252c93537bf4d4f30a10
rafig-ada <rjavadov7852@ada.edu.az>
2023-04-05 18:44:04 +0400

    QoL Changes

--- Backend/sdpprojectay2/src/main/java/com/java/sdpprojectay2/repository/ReportRepository.java
+++ Backend/sdpprojectay2/src/main/java/com/java/sdpprojectay2/repository/ReportRepository.java
@@ -4,6 +4,7 @@ import com.java.sdpprojectay2.model.entity.Report;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+
 public interface ReportRepository extends CrudRepository<Report, Integer> {
     List<Report> findAll();
 }
